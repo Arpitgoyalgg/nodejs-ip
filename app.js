084ad6e21fc57974b655609da599d6f8354c4a4b
Arpit Goyal <arpitgoyalgg@pop-os.localdomain>
2021-04-08 11:32:54 +0530

    Done porject

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const port = 8000;
+
+const RequestIp = require("@supercharge/request-ip");
+
+const expressMiddleware = function (req, res, next) {
+  req.ip = RequestIp.getClientIp(req);
+  console.log(req.ip);
+  res.send(req.ip);
+  next();
+};
+
+app.use(expressMiddleware);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
