d74cc7cec9d18d2076d52b26e6aa6af40bdd2d87
arpitgoyalgg <arpitgoyalgg@gmail.com>
2021-04-10 13:25:23 +0530

    remove unnecessary logs and strings

--- app.js
+++ app.js
@@ -6,15 +6,13 @@ const RequestIp = require("@supercharge/request-ip");
 
 const expressMiddleware = function (req, res, next) {
   req.ip = RequestIp.getClientIp(req);
-  console.log(req.ip);
-  res.send(req.ip);
   next();
 };
 
 app.use(expressMiddleware);
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send(req.ip);
 });
 
 app.listen(port, () => {
